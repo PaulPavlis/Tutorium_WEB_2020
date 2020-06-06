d5d2031e66f9cfc3f6ba77f49221db94f57dc3de
PaulPavlis <65902515+PaulPavlis@users.noreply.github.com>
2020-06-06 19:02:20 +0200

    start_template

--- PHP_CokSes_StudentVersion/ajax_sender.php
+++ PHP_CokSes_StudentVersion/ajax_sender.php
@@ -8,29 +8,16 @@
     <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+
     <title>PHP Tutorium</title>
 </head>
 
 <body>
     <div class="container mt-5">
-        <form>
-            <div class="form-group">
-                <label for="username_field">Username</label>
-                <input type="text" class="form-control" id="username_field" name="username">
-            </div>
-            <div class="form-group">
-                <label for="password_field">Password</label>
-                <input type="password" class="form-control" id="password_field" name="password">
-            </div>
-            <div class="form-group form-check">
-                <input type="checkbox" class="form-check-input" id="useless_button" name="useless">
-                <label class="form-check-label" for="useless_button">Useless af</label>
-            </div>
-            <button type="submit" class="btn btn-primary">Login</button>
-        </form>
+        <h1 id="test">Hello, Ajax!</h1>
+        <button class="btn btn-primary mt-5" id="loadAjax">Click me</button>
+        <p id="inputAjax"></p>
     </div>
-
-
 
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
--- PHP_CokSes_StudentVersion/greeting.php
+++ PHP_CokSes_StudentVersion/greeting.php
@@ -13,24 +13,8 @@
 
 <body>
     <div class="container mt-5">
-        <form>
-            <div class="form-group">
-                <label for="username_field">Username</label>
-                <input type="text" class="form-control" id="username_field" name="username">
-            </div>
-            <div class="form-group">
-                <label for="password_field">Password</label>
-                <input type="password" class="form-control" id="password_field" name="password">
-            </div>
-            <div class="form-group form-check">
-                <input type="checkbox" class="form-check-input" id="useless_button" name="useless">
-                <label class="form-check-label" for="useless_button">Useless af</label>
-            </div>
-            <button type="submit" class="btn btn-primary">Login</button>
-        </form>
+        Hello Random Person
     </div>
-
-
 
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
