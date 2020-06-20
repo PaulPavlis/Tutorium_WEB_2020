79557c17017e61ba4ae4b8cacac54fcb1ad4defa
PaulPavlis <65902515+PaulPavlis@users.noreply.github.com>
2020-06-20 09:37:20 +0200

    added start template for last years exam

---
+++ FinalProject_StartTemplate/conf/properties.php
@@ -0,0 +1,11 @@
+<?php
+
+/* 
+ * Change properties!
+ */
+
+define("HOST", "localhost");
+define("DBUSER", "root");
+define("DBPWD", "");
+define("DBNAME", "bwi_web_ss19_termin1");
+
---
+++ FinalProject_StartTemplate/index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>Web Engineering Termin 1</title>
+
+    <!-- Bootstrap -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <!-- Custom CSS -->
+    <link rel="stylesheet" href="res/css/produktestyle.css">
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+
+</head>
+
+<body>
+    <header class="page-header">
+        <img src="res/pics/Logo.jpg" alt="Logo">
+        <div class="float-right">
+            <img src="res/pics/Cart.png" alt="Cart">
+        </div>
+    </header>
+
+    <div class="container">
+        <main>
+            <h1>Willkommen im Smart Home Online Shop!</h1>
+        </main>
+    </div>
+
+</body>
+
+</html>
